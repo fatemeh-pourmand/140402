432deb6873ca33ccca65d1e8b27169bb932a834e
fatemeh-pourmand <fatemeepourmand@gmail.com>
2025-09-29 14:42:05 +0330

    Create Projct1.py
                                                                                  👍: در پروژه شماره  
    بازنویسی Class Device  و 
    اضافه کردن قابلیت camera code38
                                                                             👍   :هدف از انجام این پروژ  برنامه نویسی شی گرا در پایتون و ایجاد یک کلاس مدیریت دستگاه با قابلیت فعال سازی دوربین

---
+++ Projct1.py
@@ -0,0 +1,8 @@
+class Device:
+      self.device_name=device_name
+    
+    def __init__(self,location,group,device_type,device_name):
+        self.location=location
+        self.group=group
+        self.device_type=device_type
+        self.device_name=device_name
